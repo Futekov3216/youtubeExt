7b0c4cfb048049096580ee77e5c952a8778cb23e
Futekov3216 <noodle.hell@gmail.com>
2019-12-31 14:10:21 +0200

    zdr

---
+++ borderify.js
@@ -0,0 +1,7 @@
+console.log("START")
+
+document.getElementById('search-form').style = "transform:scale(2)"
+document.getElementsByClassName('gstl_50 sbdd_a')[0].style.backgroundColor = 'red'
+// myElement.style = "transform:scale(2)"
+console.log("END")
+
